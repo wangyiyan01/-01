74e93f3f8f11389ebc6f403cd6c5fc9e96135e6c
王以彦 <97866448+wangyiyan01@users.noreply.github.com>
2022-07-20 18:56:39 +0800

    Add files via upload
    更新版--加入了html测试报告

--- 里诺云登陆测试unittest框架.py
+++ 里诺云登陆测试unittest框架.py
@@ -3,8 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-
-
+from HTMLTestRunner import HTMLTestRunner
 class testLogin(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -17,6 +16,7 @@ class testLogin(unittest.TestCase):
         self.browser.find_element(By.ID, "password").clear()
         self.browser.find_element(By.ID, "password").send_keys(password)
         self.browser.execute_script("arguments[0].click();", self.browser.find_element(By.ID, "btnOk"))
+
     def Load(self):
         loading = self.browser.find_element(By.CSS_SELECTOR, "div.ok-loading")
         WebDriverWait(self.browser,20).until(expected_conditions.invisibility_of_element(loading))
@@ -25,19 +25,21 @@ class testLogin(unittest.TestCase):
     def test1(self):
         self.Login('admin', '123456')
         self.Load()
-        try:
-            msg = self.browser.find_element(By.CLASS_NAME, "userName").text
-        except:
-            msg = ""
-        assert 'admin' in msg
+        msg = self.browser.find_element(By.CLASS_NAME, "userName").text
+        self.assertIn("admin",msg)
 
     def test2(self):
         self.Login('admin1', '123456')
-        try:
-            msg = self.browser.find_element(By.CLASS_NAME, "layui-layer-content.layui-layer-padding").text
-        except:
-            msg = None
-        assert msg == "用户名或密码错误"
+        msg = self.browser.find_element(By.CLASS_NAME, "layui-layer-content.layui-layer-padding").text
+        self.assertEqual(msg,"用户名或密码错误")
 
     def tearDown(self):
         self.browser.quit()
+
+
+testsuite = unittest.TestLoader().loadTestsFromTestCase(testLogin) #从测试类中加载测试用例
+# runner=unittest.TextTestRunner()  #创建执行对象
+handles=open(r"d:\report\20220720.html","w",encoding='utf-8')
+runner=HTMLTestRunner(stream=handles,title="自动化测试",description="里诺云登录测试")
+runner.run(testsuite) #执行
+
